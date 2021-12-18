0de36fb5b9483e3467160313e91f18fc16dc0d70
Greg Webber <greg@jnickel.com>
2021-12-18 19:51:30 -0400

    fix inline images issue

--- src/components/2021/Week14.tsx
+++ src/components/2021/Week14.tsx
@@ -26,42 +26,52 @@ export const Week14 = ()=>{
             <li>You never under any circumstance help The Insufferable RuCunt with fantasy.</li>
         </ol>
         <p>Ladies and gentlemen... I give you The Mutts</p>
-        <LazyImg className = 'inline' src = {screenshot} alt ='Reservoir Dogs'/> 
-        <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        <div className = 'inline-image-wrapper'>
+            <LazyImg className = 'inline' src = {screenshot} alt ='Reservoir Dogs'/> 
+            <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        </div>
         <p>Aside from the incredible "Brewski" (it's spelled Bruschi RuCunt! For the last time it's Bruschi! I'm going to loose it!!) name recommendation that I hear NPC has legitimately been considering for his first born for the past year, one can't help but see in this tiny little picture The Mutts breaking every single rule of the unwritten rules of The Tuna Bowl. Right in NPC's face to boot! Every single one of them!</p>
         <p>Well slow down TBRW, would it really have made a difference? I mean come on. Latavius Murray hasn't had a carry in 4 weeks, what could possibly happen?</p>
-        <LazyImg className = 'inline' src = {image} alt ='Rupe winning'/> 
-        <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        <div className = 'inline-image-wrapper'>
+            <LazyImg className = 'inline' src = {image} alt ='Rupe winning'/> 
+            <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        </div>
         <br/>
         <br/>
         <br/>
-        <LazyImg className = 'inline' src = {score_1} alt ='Rupe winning again'/> 
-        <LazyImg className = 'inline' src = {jeffVince} alt ='Jeff-Vince'/> 
+        <div className = 'inline-image-wrapper'>
+            <LazyImg className = 'inline' src = {score_1} alt ='Rupe winning again'/> 
+            <LazyImg className = 'inline' src = {jeffVince} alt ='Jeff-Vince'/> 
+        </div>
         <br/>
         <br/>
         <br/>
-        <LazyImg className = 'inline' src = {score_2} alt ='Rupe winning again'/> 
-        <LazyImg className = 'inline' src = {jeffVince} alt ='Jeff-Vince'/> 
+        <div className = 'inline-image-wrapper'>
+            <LazyImg className = 'inline' src = {score_2} alt ='Rupe winning again'/> 
+            <LazyImg className = 'inline' src = {jeffVince} alt ='Jeff-Vince'/> 
+        </div>
         <br/>
         <br/>
         <br/>
-        <LazyImg className = 'inline' src = {score_3} alt ='Rupe winning again'/> 
-        <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        <div className = 'inline-image-wrapper'>
+            <LazyImg className = 'inline' src = {score_3} alt ='Rupe winning again'/> 
+            <LazyImg className = 'inline' src = {daveVince} alt ='Dave-Vince'/> 
+        </div>
         <br/>
         <p>JESUS CHRIST MUTTS! Don't you understand the ramifications of your actions! We already had Whitey hand pick all his keepers for him, do we really need you setting his lineup for him too!!? Week 14! Right before playoffs, with so many peoples lives on the line!? Gillies has a family, Mutts! This was his year. Jonathan Taylor Thomas! This Just In Herberts Great! Even OBJ was starting to play football. Now Gillies has to explain to his children why Daddy's face is on No Playoffs Club's logo because he's officially missed the playoffs 3 years in a row and NPC needs a new mascot! Unforgivable. Not to mention ripping 1st place from NPC and handing it to the RuCunt on a fucking silver platter! Outrageous.</p>
-        <LazyImg className = 'inline' src = {comingSoon} alt ='Coming Soon'/> 
+        <LazyImg src = {comingSoon} alt ='Coming Soon'/> 
         <p>And for the record, this isn't some kind of last ditch effort of NPC to avoid his epic failure securing 1st place down the stretch, OK. Sometimes you just have to put a mutt down. NPC told me not to bring this up but I see and hear everything and I couldn't let this stand. Unforgivable Mutts. Unforgivable.</p>
         <p>Now that that ugliness is out of the way, let's recap what was one hell of a Tuna Bowl season.</p>
         <p>Congratulations to the Mutts, I mean RuCunt on winning 1st place on the season. Everyone's nightmare has come true.</p>
-        <LazyImg className = 'inline' src = {throne} alt ='Throne'/> 
+        <LazyImg src = {throne} alt ='Throne'/> 
         <p>Oh fuck I think Moran's going to be sick!</p>
-        <LazyImg className = 'inline' src = {puke} alt ='Moran once again puking'/> 
+        <LazyImg src = {puke} alt ='Moran once again puking'/> 
         <p>Ok regroup.. Rucunt's your Season Champ and Louie is your Minnow Champ with a 1-27 record. Everything in between is just a wash. Moran and The Mutts were your high scorers with 178 and 140 points respectively and face off in a monster 4th vs 5th seed showdown.</p>
         <p>Timmay capitalizing on the chance Gillies blew a Gaskin against RuCunt had maybe the most clutch move of the season when he dropped Covid Higbee before kick off and picked up the back up back up TE who I refuse to look up. Either way, he got the 2.5 points he needed to send The Nimbler Bimbler home packing and secure 6th place and a shot at the Tuna. Tim faces the 3rd seed Rondingo who's rounding out the season with an impressive 7 game win streak.</p>
         <p>Well I speak for everyone here in saying it was a hell of a season and I'm looking forward to the Playoffs. Also Congrats again to the RuCunt on an impressive seas...</p>
-        <LazyImg className = 'inline' src = {puke} alt ='Moran once again puking'/>
+        <LazyImg src = {puke} alt ='Moran once again puking'/>
         <p>You know what! I can't go on! I vote Death by Arrows for The Mutts!</p>
-        <LazyImg className = 'inline' src = {arrows} alt ='Death by arrows!'/>
+        <LazyImg src = {arrows} alt ='Death by arrows!'/>
         <p>Good luck and set those lineups!</p>
     </Recap>
 }
