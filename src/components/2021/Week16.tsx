372724cd75955634ac1cc9b93a87f5b1f1e3a27c
Greg Webber <greg@jnickel.com>
2021-12-29 17:22:58 -0400

    scrape week 16 data and recap wrapper

---
+++ src/components/2021/Week16.tsx
@@ -0,0 +1,10 @@
+import LazyImg from '../LazyImg';
+import { Recap } from '../Recap';
+import { WEEK_MARKERS } from '../../icons';
+
+export const Week16 = ()=>{
+    const title = <>{}Round 2 {}</>
+    return <Recap title = {title} weekInt = {16} exclude = {[2,3,4,5]}>
+        
+    </Recap>
+}
